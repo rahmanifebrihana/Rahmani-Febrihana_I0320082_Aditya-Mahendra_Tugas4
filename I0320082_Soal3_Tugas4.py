d19d53ad9be7f6d730518e375623b5873e22a0a3
rahmani <rfebrihana@gmail.com>
2021-03-26 01:53:31 -0800

    Pengerjaan Exercise

---
+++ I0320082_Soal3_Tugas4.py
@@ -0,0 +1,4 @@
+p = 50
+q = int(input("Memasukkan berat bagasi :"))
+print(q <= p)
+
---
+++ I0320082_Soal6_Tugas4.py
@@ -0,0 +1,8 @@
+p = 4
+q = 11
+print(p|q , bin(p|q))
+print(p>>q , bin(p>>q))
+print(p^q , bin(p^q))
+print(~p , bin(~p))
+print(q & p)
+
