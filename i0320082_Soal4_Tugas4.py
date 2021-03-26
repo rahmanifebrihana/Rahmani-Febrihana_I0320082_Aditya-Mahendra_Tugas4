6d5570ec43a28caf505737397325c5dafdb730e6
rahmani <rfebrihana@gmail.com>
2021-03-26 02:04:58 -0800

    Pengerjaan Soal

---
+++ i0320082_Soal4_Tugas4.py
@@ -0,0 +1,12 @@
+usia = 21
+p = int(input("Berapa usia anda : "))
+if usia <= p :
+    print("Anda memenuhi usia")
+    q = str(input("Apakah anda lulus ujian kualifikasi (Y/T) "))
+    if q == "Y" :
+        print("Anda boleh mengikuti kursus")
+    elif q == "T" :
+        print("Anda tidak boleh mengikuti kursus")
+
+else :
+    print("Anda tidak memenuhi usia")
